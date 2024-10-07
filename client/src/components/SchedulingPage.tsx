f1d5b78746ca9583b7ee70f94ddff713ad3d7c8d
Juan Mireles <juancnmireles@gmail.com>
2024-10-07 19:09:43 -0400

    Start Scheduling Page

---
+++ client/src/components/SchedulingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+
+const SchedulingPage = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    return (
+        <div>
+            <h1>Scheduling Page</h1>
+        </div>
+    );
+};
+
+export default SchedulingPage;
