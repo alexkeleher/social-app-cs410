41deb7dddfdf407eea737ef073f9f878161a93d8
alexkeleher <87454086+alexkeleher@users.noreply.github.com>
2024-11-02 18:54:05 -0400

    added creation of user_sessions table to database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -69,6 +69,14 @@ CREATE TABLE UserRestaurantTypeXRef (
 	FOREIGN KEY (RestaurantTypeID) REFERENCES RestaurantType(ID) ON DELETE CASCADE
 );
 
+CREATE TABLE user_sessions (
+  sid varchar NOT NULL COLLATE "default",
+  sess json NOT NULL,
+  expire timestamp(6) NOT NULL
+) WITH (OIDS=FALSE);
+
+ALTER TABLE "user_sessions" ADD CONSTRAINT user_sessions_pkey PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
 
 -- Insert Dummy Data into Users Table
 INSERT INTO Users(
